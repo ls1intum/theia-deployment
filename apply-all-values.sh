e1f5eb1ca92e3621d92d67ce5648b508dba27615
Auxburger <darius.augsburger@vulpery.com>
2025-07-13 12:28:40 +0200

    define eager start in operator and use supported version

---
+++ apply-all-values.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+namespace=$1
+if [ -z "$namespace" ]; then
+  echo "Using default namespace 'theia-prod'."
+  namespace="theia-prod"
+fi
+
+helm repo add theia-cloud-repo https://eclipse-theia.github.io/theia-cloud-helm/
+helm repo update
+
+helm upgrade theia-cloud-base theia-cloud-repo/theia-cloud-base --install --namespace $namespace -f theia-base-helm-values.yml
+
+helm upgrade theia-cloud-crds theia-cloud-repo/theia-cloud-crds --install --namespace $namespace -f theia-crds-helm-values.yml
+
+helm upgrade theia-cloud theia-cloud-repo/theia-cloud --install --namespace $namespace -f theia-cloud-helm-values.yml
+
