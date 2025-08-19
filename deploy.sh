01a5c14c44dad6b4d32c32b1330bdd401e636b38
Matthias Linhuber <matthias.linhuber@tum.de>
2025-08-19 17:04:57 +0200

    Further Cleanup

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ if [ -z "$namespace" ]; then
   namespace="theia-prod"
 fi
 
-helm dependency update ./tum-theia-cloud
+helm dependency update ./charts/tum-theia-cloud
 
 helm upgrade theia-cloud-base theia-cloud-repo/theia-cloud-base --install -f theia-base-helm-values.yml
 helm upgrade theia-cloud-crds theia-cloud-repo/theia-cloud-crds --install -f theia-crds-helm-values.yml
 
 # this installs the latest version of theia-cloud, theia-appdefinitions and theia-certificates
-helm upgrade --install tum-theia-cloud ./tum-theia-cloud --namespace $namespace --create-namespace \
+helm upgrade --install tum-theia-cloud ./charts/tum-theia-cloud --namespace $namespace --create-namespace \
   --theia-certificates.wildcardCertificate="$(cat ./prod/wildcard-webview-cert/wildcard_webview_instance_theia_artemis_cit_tum_de.pem)" \
   --theia-certificates.wildcardKey="$(cat ./prod/wildcard-webview-cert/wildcard_webview_instance_theia_artemis_cit_tum_de.key)"
